3337ee02406d867fd94fdae1d9b279a8f4437d56
unknown <satya-1298>
2023-07-06 14:52:22 +0530

    [Satya]Add : inserting Values in list using inbuilt methods

---
+++ LinkedList_Prog/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList_Prog
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Linked List Program");
+            ListData listData = new ListData();
+            listData.Add();
+            Console.ReadLine();
+
+            
+        }
+    }
+}
